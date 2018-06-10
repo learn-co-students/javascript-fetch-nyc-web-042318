fd13349999b22f9aa586c8efb7bb7a3b1427f8bc
ensallee <ensallee@gmail.com>
2018-06-10 12:17:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const token = 'eabbe22816e571098b87faa480ce4c55c84286fa'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(function(json) {
+  json.forEach(function(obj) {
+    let el = document.createElement('p')
+    el.innerHTML = `Repo name: ${obj.name}`
+    document.body.appendChild(el)
+  })
+} );
+
+
+// console.log(json)
